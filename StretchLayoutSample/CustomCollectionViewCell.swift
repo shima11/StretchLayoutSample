14f16ef7a8265ac4f02a2c496c7be6fee08df2d3
shima <shima11@users.noreply.github.com>
2017-01-25 11:12:11 +0900

    add collection view cell

---
+++ StretchLayoutSample/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  StretchLayoutSample
+//
+//  Created by shima jinsei on 2017/01/24.
+//  Copyright © 2017年 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
